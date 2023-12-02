e875a79d917bfc8fb1bcffbe1f1f4e460422e423
Qinxi <qinxiw2@illinois.edu>
2023-12-02 15:54:19 -0800

    rf

---
+++ Model/classification_recommender.py
@@ -0,0 +1,12 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+
+
+def main():
+
+
+
+if __name__ == '__main__':
+    main()
